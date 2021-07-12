ccca858eca47a328450cf5cefc7103ffe0ad23bb
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-07-11 20:24:52 -0700

    created controlled inputs

--- playlist-generator/src/components/AddTrack.js
+++ playlist-generator/src/components/AddTrack.js
@@ -1,21 +1,32 @@
+import { useState } from "react"
+
 export default function AddTrack(){
+    const [form, setForm] = useState({
+        songName: "",
+        movieTitle: "",
+        duration: "",
+    })
+    const handleOnInputChange = (event) => {
+        setForm({ ...form, [event.target.name]: event.target.value})
+        
+    }
     return(
         <div className="add-track-form">
             <h2> Add New Track </h2>
 
             <div className="form-input">
                 <label htmlFor="songName">Song Name </label>
-                <input name="songName" type="text" placeholder="song name" />
+                <input name="songName" type="text" placeholder="song name" value={form.songName} onChange={handleOnInputChange}/>
             </div>
 
             <div className="form-input">
                 <label htmlFor="movieTitle">Movie Title </label>
-                <input name="movieTitle" type="text" placeholder="movie title" />
+                <input name="movieTitle" type="text" placeholder="movie title" value={form.movieTitle} onChange={handleOnInputChange}/>
             </div>
 
             <div className="form-input">
                 <label htmlFor="duration">Duration in Seconds </label>
-                <input name="duration" type="number"/>
+                <input name="duration" type="number" value={form.duration} onChange={handleOnInputChange}/>
             </div>
 
             <button className="btn submit">Save</button>
